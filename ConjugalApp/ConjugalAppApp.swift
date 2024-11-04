0a48db205fb129a4600751a500e6ed09d177b6ce
Kanishka Chaudhry <kanishka.chaudhry@grofers.com>
2024-11-04 21:40:54 +0530

    Initial Commit

---
+++ ConjugalApp/ConjugalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConjugalAppApp.swift
+//  ConjugalApp
+//
+//  Created by Kanishka Chaudhry on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ConjugalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
